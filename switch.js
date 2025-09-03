2262e9fa24a1087ce1afc794b7f83913c1368a1b
KondalaraoRuttala <131016349+KondalaraoRuttala@users.noreply.github.com>
2025-09-03 10:49:22 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,24 @@
+let  N=3
+switch (N)
+{
+    case 1:
+        console.log('good');
+        break;
+     case 2:
+            console.log(' verygood');
+            break;
+     case 3:
+        console.log('bad');
+        break;
+    case 4:
+         console.log('excellent');
+        break;
+    case 5:
+     console.log('to bad');
+        break;
+
+
+    default:
+        console.log('no one');
+
+}
